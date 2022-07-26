f5296e25f7952fdfa7a3e23df0e2d99c02d622cc
Mohammed Busaidy <m.busaidy@live.com>
2022-07-26 16:58:23 +0200

    changed datajs works except of image

--- src/data.js
+++ src/data.js
@@ -1,12 +1,16 @@
-const data = {
-  product: [
-    {
-      id: '1',
-      name: 'a7000',
-      price: '1700',
-      image: 'public/images/a7000.png',
-    },
-  ],
-};
+const data = [
+  {
+    id: '1',
+    name: 'a7000',
+    price: '1700',
+    image: 'public/images/a7000.png',
+  },
 
+  {
+    id: '2',
+    name: 'a7000',
+    price: '1700',
+    image: 'public/images/a7000.png',
+  },
+];
 export default data;
